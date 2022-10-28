eebf354df0c3745e60deeba7b6b22874931983f5
sergey.churkov <sergey.churkov@g5e.com>
2022-10-28 15:29:49 +0300

    add static script and server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socket = require('socket.io')
 const app = express()
+const server = http.createServer(app)
 const publicPath = path.join(__dirname, '/../public')
-const PORT = 3000
-
+const PORT = process.env.PORT || 3000
+const io = socket(server)
 app.use(express.static(publicPath))
-app.listen(PORT,()=>{
+
+io.on('connection', (socket) => {
+    console.log('a new user just connection')
+    socket.on('disconnect', () => {
+        console.log('user was disconnected')
+    })
+})
+
+
+
+server.listen(PORT, () => {
     console.log(`Server running on ${PORT} port`)
 })
 
